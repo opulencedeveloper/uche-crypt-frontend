656963692bab896550ce203db2549ea41f4a5e2a
unknown <pebuka351@gmail.com>
2024-07-25 22:23:50 +0100

    Implemented Desktop and Mobile View Designs and Integrated News letter API

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,12 +1,9 @@
-import AuthenticationLayout from "@/layouts/authentication";
 import ForgotPasswordForm from "@/components/application/forgot-password/form";
 
 export default function Page() {
   return (
-    <AuthenticationLayout>
-      <section className="w-full h-max overflow-y-auto max-h-full flex flex-col items-center">
-        <ForgotPasswordForm />
-      </section>
-    </AuthenticationLayout>
+    <section className="w-full h-max overflow-y-auto max-h-full flex flex-col items-center">
+      <ForgotPasswordForm />
+    </section>
   );
 }
--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,12 +1,9 @@
-import AuthenticationLayout from "@/layouts/authentication";
 import ResetPasswordForm from "@/components/application/reset-password/form";
 
 export default function Page() {
   return (
-    <AuthenticationLayout>
-      <section className="w-full h-max flex flex-col items-center">
-        <ResetPasswordForm />
-      </section>
-    </AuthenticationLayout>
+    <section className="w-full h-max flex flex-col items-center">
+      <ResetPasswordForm />
+    </section>
   );
 }
